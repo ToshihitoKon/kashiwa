1322ac0cc38ca12fb3cc4f9892add6d54e0ec3c5
Toshihito Kon <toshihito.kon@gmail.com>
2020-05-14 21:51:07 +0900

    add chocho

--- src/components/pages/Music.vue
+++ src/components/pages/Music.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container mt-4">
+    <Chocho />
     <Player />
     <Queuelist />
     <Playlist />
@@ -10,9 +11,11 @@
 import Player from '@/components/music/Player.vue'
 import Queuelist from '@/components/music/Queuelist.vue'
 import Playlist from '@/components/music/Playlist.vue'
+import Chocho from '@/components/music/Chocho.vue'
 
 export default {
   components: {
+    Chocho,
     Player,
     Queuelist,
     Playlist,
