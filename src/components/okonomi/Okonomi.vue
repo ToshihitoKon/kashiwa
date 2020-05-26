79d21a59d27c235ecab51660d2898d7575896561
Toshihito Kon <toshihito.kon@gmail.com>
2020-05-26 09:35:37 +0900

    add okonomi

---
+++ src/components/okonomi/Okonomi.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    key
+    value
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    Chocho,
+    Player,
+    Queuelist,
+    Playlist,
+  }
+</script>
---
+++ src/components/pages/Okonomi.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="mt-2">
+  key
+  value
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
