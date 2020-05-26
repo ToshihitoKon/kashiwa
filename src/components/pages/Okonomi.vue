860cc0768524252413cd98d1341846e1cbc4a12f
Toshihito Kon <toshihito.kon@gmail.com>
2020-05-26 09:45:02 +0900

    雑にスイッチ

--- src/components/pages/Okonomi.vue
+++ src/components/pages/Okonomi.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="mt-2">
-  key
-  value
+    <button
+      v-on:click="toggle_ziho"
+      class="btn btn-light">
+      toggle genkai
+    </button>
+    {{ zihouStatus }}
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+import Axios from 'axios'
+
 export default {
-  components: {
+  data: function(){
+    return {
+      response: {}
+    }
+  },
+  computed: {
+    ...mapState('constants', {
+      apiUrl: state => state.apiUrl,
+    }),
+    zihouStatus: function(){
+      return (this.response.value == 0 )?'有効':'無効'
+    }
+  },
+  methods: {
+    toggle_ziho: function(){
+      Axios.post(`${this.apiUrl}/okonomi/toggle`,{
+          key: "ZIHOU"
+        })
+        .then(function(res){
+          this.response = res.data
+        }.bind(this))
+
+    }
   }
 }
 </script>
