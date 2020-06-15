949d521ad1b2e70dbf31113df8cd42a1cbcb6a3d
Toshihito Kon <toshihito.kon@gmail.com>
2020-06-15 21:17:19 +0900

    fix header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,15 +7,21 @@
       <router-link to='/' class="navbar-brand">Kashiwa</router-link>
       <!-- メニュー -->
       <div id="sidebartoggle" class="collapse navbar-collapse">
-        <Player />
-        <div>
-          <router-link to='/music' class="navbar-text">music</router-link>
-        </div>
-        <div>
-          <router-link to='/daifuku' class="navbar-text">daifuku</router-link>
-        </div>
-        <div>
-          <router-link to='/okonomi' class="navbar-text">okonomi</router-link>
+        <div class="row">
+          <div class="col col-3">
+            <div>
+              <router-link to='/music' class="navbar-text">music</router-link>
+            </div>
+            <div>
+              <router-link to='/daifuku' class="navbar-text">daifuku</router-link>
+            </div>
+            <div>
+              <router-link to='/okonomi' class="navbar-text">okonomi</router-link>
+            </div>
+          </div>
+          <div class="col col-md-auto">
+            <Player />
+          </div>
         </div>
       </div>
     </nav>
