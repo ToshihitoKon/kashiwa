37edfaf2d6fa0d555198f513678e6d6c79f6df31
Toshihito Kon <toshihito.kon@gmail.com>
2020-11-15 23:39:37 +0900

    カラムでスクロール

--- src/components/music/PlaylistItem.vue
+++ src/components/music/PlaylistItem.vue
@@ -1,17 +1,19 @@
 <template>
-  <li style="display: inline-block">
-    <div class="list-group" style="min-width: 180px">
-      <button
-        v-for="(entry, i) in entries"
-        :key="index + i +  entry"
-        class="list-group-item list-group-item-action"
-        :class="{ active: isSelectedItem(entry) }"
-        v-on:click="selectItem(entry)"
-        href="#">
-        {{ entry }}
-      </button>
-    </div>
-  </li>
+  <div style="display: inline-block; height: 80vh; overflow: auto">
+    <li class="list-group">
+      <div style="min-width: 180px; ">
+        <button
+          v-for="(entry, i) in entries"
+          :key="index + i +  entry"
+          class="list-group-item list-group-item-action"
+          :class="{ active: isSelectedItem(entry) }"
+          v-on:click="selectItem(entry)"
+          href="#">
+          {{ entry }}
+        </button>
+      </div>
+    </li>
+  </div>
 </template>
 
 <script>
